344fc98d0216d62b8216def0776e481beaca2990
LukasGon07 <goncalves.isabela08@aluno.ifce.edu.br>
2026-01-30 16:42:34 -0300

    Refactor lista.h to use function declarations
    Removed function implementations and added function declarations for creating and manipulating a linked list.

--- DisciplinaED/Aula2/Lista/lista.h
+++ DisciplinaED/Aula2/Lista/lista.h
@@ -9,38 +9,10 @@ typedef struct No {
     struct No *proximo;
 } No;
 
-No* criarNo(int valor) {
-    No* novo = (No*) malloc(sizeof(No));
-    novo->valor = valor;
-    novo->proximo = NULL;
-    return novo;
-}
+No* criarNo(int valor); 
 
-void inserirInicio(No **lista, int valor) {
-    No* novo = criarNo(valor);
-    novo->proximo = *lista;
-    *lista = novo;
-}
+void inserirInicio(No **lista, int valor);
 
-void imprimirLista(No *lista) {
-    while (lista != NULL) {
-        printf("%d -> ", lista->valor);
-        lista = lista->proximo;
-    }
-    printf("NULL\n");
-}
+void imprimirLista(No *lista);
 
-int main() {
-    No* lista = NULL;
-
-    inserirInicio(&lista, 10);
-    inserirInicio(&lista, 20);
-    inserirInicio(&lista, 30);
-
-    imprimirLista(lista);
-
-    return 0;
-}
-
-#include "Lista.c"
 #endif
