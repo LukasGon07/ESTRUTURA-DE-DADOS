586ceb5d8fe0f825ff51c9dab06f397b4079c21c
LukasGon07 <goncalves.isabela08@aluno.ifce.edu.br>
2026-01-30 16:27:15 -0300

    Update lista.h

--- DisciplinaED/Aula2/Lista/lista.h
+++ DisciplinaED/Aula2/Lista/lista.h
@@ -1,6 +1,46 @@
-#ifndef__LISTA_H___
-#define __LISTA_H___
+#ifndef Lista_H
+#define Lista_H
+
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef struct No {
+    int valor;
+    struct No *proximo;
+} No;
+
+No* criarNo(int valor) {
+    No* novo = (No*) malloc(sizeof(No));
+    novo->valor = valor;
+    novo->proximo = NULL;
+    return novo;
+}
+
+void inserirInicio(No **lista, int valor) {
+    No* novo = criarNo(valor);
+    novo->proximo = *lista;
+    *lista = novo;
+}
+
+void imprimirLista(No *lista) {
+    while (lista != NULL) {
+        printf("%d -> ", lista->valor);
+        lista = lista->proximo;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    No* lista = NULL;
+
+    inserirInicio(&lista, 10);
+    inserirInicio(&lista, 20);
+    inserirInicio(&lista, 30);
+
+    imprimirLista(lista);
+
+    return 0;
+}
+
 #include "Lista.c"
 #endif
